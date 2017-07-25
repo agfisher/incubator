b0a08f77c03be0e4b3dedc5f2b666e1e526dcd5d
agfisher <alexey.fisher@gmail.com>
2017-07-25 15:26:00 +0300

    initial

---
+++ src/main/java/com/reserv/IncubatorApplication.java
@@ -0,0 +1,19 @@
+package com.reserv;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by kash on 29.06.2017.
+ */
+@ApplicationPath("/")
+public class IncubatorApplication extends Application {
+    @Override
+    public Set<Class<?>> getClasses() {
+        HashSet h = new HashSet<Class<?>>();
+        h.add( IncubationDataController.class );
+        return h;
+    }
+}
