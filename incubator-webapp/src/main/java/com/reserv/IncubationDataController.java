fac31afdb5b7e02a80762f465b781e56d893ebfd
Андрей Кашулин <awkashulin@gmail.com>
2017-10-08 23:10:57 +0300

    Fix data

--- incubator-webapp/src/main/java/com/reserv/IncubationDataController.java
+++ incubator-webapp/src/main/java/com/reserv/IncubationDataController.java
@@ -2,14 +2,15 @@ package com.reserv;
 
 import com.reserv.model.DataModel;
 
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
 /**
- * Created by kash on 29.06.2017.
+ * Created by kash on 29.069.2017.
  */
-@Path("/")
+@Path("/rest")
 public class IncubationDataController {
     @GET
     @Produces("application/json")
